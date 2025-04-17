1a46ff2341dee68efc1742a3367dc15fdd52c650
AiNi1823 <landou_01@163.com>
2025-04-17 10:45:03 +0800

    ch3 update

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -14,7 +14,7 @@ mod switch;
 #[allow(clippy::module_inception)]
 mod task;
 
-use crate::config::MAX_APP_NUM;
+use crate::config::{MAX_APP_NUM,MAX_CALL_NUM};
 use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
 use lazy_static::*;
@@ -54,6 +54,7 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            sys_call_num:[0; MAX_CALL_NUM], 
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -64,7 +65,7 @@ lazy_static! {
             inner: unsafe {
                 UPSafeCell::new(TaskManagerInner {
                     tasks,
-                    current_task: 0,
+                    current_task: 0,// Initialize the task call number array
                 })
             },
         }
@@ -135,6 +136,18 @@ impl TaskManager {
             panic!("All applications completed!");
         }
     }
+
+    fn add_syscall_num(&self, syscall_id: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].sys_call_num[syscall_id] += 1;
+    }
+
+    fn get_syscall_num(&self, syscall_id: usize) -> usize {
+        let inner = self.inner.exclusive_access();
+        let current: usize = inner.current_task;
+        inner.tasks[current].sys_call_num[syscall_id]
+    }
 }
 
 /// Run the first task in task list.
@@ -158,6 +171,17 @@ fn mark_current_exited() {
     TASK_MANAGER.mark_current_exited();
 }
 
+/// Add syscall number to the current task
+pub fn add_syscall_num(syscall_id: usize) {
+    TASK_MANAGER.add_syscall_num(syscall_id);
+}
+
+/// Get the current task calls number
+pub fn get_syscall_num(syscall_id: usize) -> usize {
+    let num = TASK_MANAGER.get_syscall_num(syscall_id);
+    num 
+}
+
 /// Suspend the current 'Running' task and run the next task in task list.
 pub fn suspend_current_and_run_next() {
     mark_current_suspended();
@@ -169,3 +193,4 @@ pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
 }
+
--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,6 +1,7 @@
 //! Types related to task management
 
 use super::TaskContext;
+use crate::config::MAX_CALL_NUM;
 
 /// The task control block (TCB) of a task.
 #[derive(Copy, Clone)]
@@ -9,6 +10,8 @@ pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
+    /// The task calls number
+    pub sys_call_num: [usize; MAX_CALL_NUM],
 }
 
 /// The status of a task
